79c8d0f0b5fb33ac9cd23caf4dba7117c14da904
Michael.Erickson <ylnorthville@gmail.com>
2020-10-25 14:19:39 -0400

    first commit

---
+++ loopsalgo.js
@@ -0,0 +1,11 @@
+for (var num=0; num<21; num++){
+    if (num % 2 != 0){
+        console.log(num);
+    }
+}
+
+var sum = 0
+for (var num=1; num<=5; num++){
+    sum+=num
+    console.log('num:', num, 'sum', sum);
+}
